8c5abab5a565f21161b4bf6b5bc380558729e3df
wffchong <283953455@qq.com>
2022-10-19 18:21:40 +0800

    feat: 添加content页面

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,15 +1,37 @@
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Layout } from 'antd'
 import LayoutMenu from './components/Menu'
 import LayoutHeader from './components/Header'
 import LayoutTabs from './components/Tabs'
 import LayoutFooter from './components/Footer'
-import './index.less'
-import { useSelector } from '@/store'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { useDispatch, useSelector } from '@/store'
+import { updateCollapse } from '@/store/modules/menu'
 
-const { Sider } = Layout
+import './index.less'
+
+const { Sider, Content } = Layout
 
 const LayoutIndex: React.FC = () => {
+	const dispatch = useDispatch()
 	const { isCollapse } = useSelector(state => state.menu)
+	const { pathname } = useLocation()
+
+	// 监听窗口大小变化
+	const listeningWindow = () => {
+		window.onresize = () => {
+			return (() => {
+				let screenWidth = document.body.clientWidth
+				if (isCollapse === false && screenWidth < 1200) dispatch(updateCollapse(true))
+				if (isCollapse === false && screenWidth > 1200) dispatch(updateCollapse(false))
+			})()
+		}
+	}
+
+	useEffect(() => {
+		listeningWindow()
+	}, [])
 
 	return (
 		<section className='container'>
@@ -19,6 +41,15 @@ const LayoutIndex: React.FC = () => {
 			<Layout>
 				<LayoutHeader></LayoutHeader>
 				<LayoutTabs></LayoutTabs>
+				<Content>
+					{/* TransitionGroup 会导致 useEffect 加载两次，后期在解决 */}
+					<TransitionGroup className='content'>
+						{/* exit：表示退出当前页面的时候是否有动画 */}
+						<CSSTransition key={pathname} timeout={200} classNames='fade' exit={false}>
+							<Outlet></Outlet>
+						</CSSTransition>
+					</TransitionGroup>
+				</Content>
 				<LayoutFooter></LayoutFooter>
 			</Layout>
 		</section>
